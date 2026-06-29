ff56df69ff714db409e1c4c10cdef243ccb39841
Pranav <gargpranav851@gmail.com>
2026-06-30 00:12:14 +0530

    chore(routes): mount repoRoutes under /api/repos

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 const authRoutes = require('./authRoutes');
+const repoRoutes = require('./repoRoutes');
 
 router.use('/auth', authRoutes);
+router.use('/repos', repoRoutes);
 
 module.exports = router;
